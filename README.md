bf0af99f888556ff002371d88da143706af31044
mk-pmb <mk-pmb@users.noreply.github.com>
2016-11-11 19:12:58 +0100

    v0.1.0: Array.from

---
+++ README.md
@@ -0,0 +1,25 @@
+﻿
+<!--#echo json="package.json" key="name" underline="=" -->
+epiphany-shims-pmb
+==================
+<!--/#echo -->
+
+<!--#echo json="package.json" key="description" -->
+Selected ES5+ shims for the epiphany browser.
+<!--/#echo -->
+
+
+Usage
+-----
+
+```html
+<script src="/node_modules/epiphany-shims-pmb/shims.js"></script>
+```
+
+
+
+License
+-------
+<!--#echo json="package.json" key=".license" -->
+ISC
+<!--/#echo -->
---
+++ shims.js
@@ -0,0 +1,37 @@
+﻿/*jslint indent: 2, maxlen: 80, continue: false, unparam: false, browser: true */
+/* -*- tab-width: 2 -*- */
+(function () {
+  'use strict';
+
+  var OPtHas = Object.prototype.hasOwnProperty;
+
+  function install(obj, key, func) {
+    if (OPtHas.call(obj, key)) { return; }
+    obj[key] = func;
+  }
+
+  install(Array, 'from', function toArray(src) {
+    return Array.prototype.slice.call(src);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return;
+}());
