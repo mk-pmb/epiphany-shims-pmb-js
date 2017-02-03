d6fc7a7471df76f5917b17d5da7170b13001374d
mk-pmb <mk-pmb@users.noreply.github.com>
2017-02-03 19:38:15 +0100

    v0.1.2: now with MSIE 6 shims

--- README.md
+++ README.md
@@ -5,13 +5,23 @@ epiphany-shims-pmb
 <!--/#echo -->
 
 <!--#echo json="package.json" key="description" -->
-Selected ES5+ shims for the epiphany browser.
+Selected ES5+ shims for the epiphany browser, and (as of 0.1.2) MSIE 6.
 <!--/#echo -->
 
-Superseded
-----------
 
-This package has been superseded by `msie6-shims-pmb`.
+MSIE 6 shims
+------------
+
+In v0.1.1 I announced I'd rename this package to `msie6-shims-pmb`.
+However, just before publishing 0.1.2, with my favorite shims for IE
+added already, I discovered I won't need them:
+After all, the `es5-shim` module actually does work in my MSIE 6,
+so better use that one as it's more complete.
+Its prior failures in my tests were due to a problem with my
+[Wine](https://www.winehq.org/) config.
+
+I'll publish them anyway in case someone can't easily use `es5-shim`
+for whatever reason.
 
 
 
@@ -20,7 +30,7 @@ Usage
 -----
 
 ```html
-<script src="/node_modules/epiphany-shims-pmb/shims.js"></script>
+<script src="/node_modules/msie6-shims-pmb/shims.js"></script>
 ```
 
 
--- shims.js
+++ shims.js
@@ -3,17 +3,54 @@
 (function () {
   'use strict';
 
-  var OPtHas = Object.prototype.hasOwnProperty;
+  var objPt = Object.prototype, OPtHas = objPt.hasOwnProperty,
+    arrPt = Array.prototype, arrSlice = arrPt.slice;
 
   function install(obj, key, func) {
-    if (OPtHas.call(obj, key)) { return; }
+    if (OPtHas.call(obj, key) && (obj[key] !== undefined)) { return; }
     obj[key] = func;
   }
 
-  install(Array, 'from', function toArray(src) {
-    return Array.prototype.slice.call(src);
+  install(Array, 'from', function toArray(src) { return arrSlice.call(src); });
+
+  function arrayMap(arr, iter, collect) {
+    var len = arr.length, idx, rslt;
+    for (idx = 0; idx < len; idx += 1) {
+      rslt = iter(arr[idx], idx, arr);
+      if (collect) { collect[idx] = rslt; }
+    }
+    return collect;
+  }
+  install(arrPt, 'forEach', function forEach(f) { return arrayMap(this, f); });
+  install(arrPt, 'map', function map(f) { return arrayMap(this, f, []); });
+
+  install(Object, 'keys', function keys(obj) {
+    if ((obj === null) || (obj === undefined)) {
+      throw new TypeError("can't convert " + String(obj) + " to object");
+    }
+    var found = [], key;
+    for (key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        // ^-- b/c jslint doesn't know about OPtHas
+        found[found.length] = key;
+      }
+    }
+    return found;
   });
 
+  install(Object, 'assign', function assign(dest, src) {
+    if ((src && typeof src) === 'object') {
+      Object.keys(src).forEach(function (k) {
+        var v = src[k];
+        if (v === undefined) { return; }
+        dest[k] = v;
+      });
+    }
+    if (arguments.length > 2) {
+      arrSlice.call(arguments, 2).forEach(function (s) { assign(dest, s); });
+    }
+    return dest;
+  });
 
 
 
